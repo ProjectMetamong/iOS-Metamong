55f145b154fb96cbcf207f1dbfb3f05b18bd1e0f
yabby <yabby1997@gmail.com>
2021-05-04 17:17:34 +0900

    [refactor] 폴더구조 변경, ViewController 이름 변경

---
+++ PoC UIKit/ViewController/ExerciseUploadViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ExerciseUploadViewController.swift
+//  PoC UIKit
+//
+//  Created by Seunghun Yang on 2021/05/04.
+//
+
+import UIKit
+
+class ExerciseUploadViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
