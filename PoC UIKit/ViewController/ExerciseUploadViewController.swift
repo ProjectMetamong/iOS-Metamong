1c665adf5fd1938d49c7f0aa150484418a3dfd3c
yabby <yabby1997@gmail.com>
2021-05-04 17:20:01 +0900

    [fix] ViewController 파일명 수정

--- PoC UIKit/ViewController/ExerciseUploadViewController.swift
+++ PoC UIKit/ViewController/ExerciseUploadViewController.swift
@@ -7,14 +7,12 @@
 
 import UIKit
 
-class ExerciseUploadViewController: UIView {
+class ExerciseUploadViewController: UIViewController {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
     }
-    */
+
 
 }
