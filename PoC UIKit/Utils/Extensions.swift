eadfe941b42b9c531037957d3a7461d770458f38
yabby <yabby1997@gmail.com>
2021-05-23 22:13:00 +0900

    ExerciseCamViewController 추가

--- PoC UIKit/Utils/Extensions.swift
+++ PoC UIKit/Utils/Extensions.swift
@@ -27,3 +27,11 @@ extension Int {
         return time
     }
 }
+
+extension UINavigationController {
+    func popToViewController(ofClass: AnyClass, animated: Bool = true) {
+        if let vc = viewControllers.last(where: { $0.isKind(of: ofClass) }) {
+            popToViewController(vc, animated: animated)
+        }
+    }
+}
--- PoC UIKit/ViewController/ExerciseCamViewController.swift
+++ PoC UIKit/ViewController/ExerciseCamViewController.swift
@@ -1,27 +1,27 @@
 //
-//  ExerciseViewController.swift
+//  ExerciseCamViewController.swift
 //  PoC UIKit
 //
-//  Created by Seunghun Yang on 2021/05/22.
+//  Created by Seunghun Yang on 2021/05/23.
 //
 
 import UIKit
 import AVKit
 import SnapKit
 
-class ExerciseReferenceViewController: UIViewController {
+class ExerciseCamViewController: UIViewController {
     
     // MARK: - Properties
     
-    lazy var referenceVideo: AVPlayer = {
+    lazy var camVideo: AVPlayer = {
         let videoURL = URL(fileURLWithPath: Bundle.main.path(forResource: "test", ofType: "mp4")!)
         let player = AVPlayer(url: videoURL)
         player.play()
         return player
     }()
     
-    lazy var referenceVideoLayer: AVPlayerLayer = {
-        let layer = AVPlayerLayer(player: self.referenceVideo)
+    lazy var camVideoLayer: AVPlayerLayer = {
+        let layer = AVPlayerLayer(player: self.camVideo)
         layer.frame = view.bounds
         layer.videoGravity = .resizeAspectFill
         return layer
@@ -35,17 +35,6 @@ class ExerciseReferenceViewController: UIViewController {
         button.setTitle("종료", for: .normal)
         button.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .heavy)
         button.addTarget(self, action: #selector(self.handleStopButtonTapped), for: .touchUpInside)
-        return button
-    }()
-    
-    lazy var startButton: UIButton = {
-        let button = UIButton()
-        button.clipsToBounds = true
-        button.layer.cornerRadius = cornerRadius
-        button.backgroundColor = .systemGreen
-        button.setTitle("시작하기", for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .heavy)
-        button.addTarget(self, action: #selector(self.handleStartButtonTapped), for: .touchUpInside)
         return button
     }()
     
@@ -77,15 +66,14 @@ class ExerciseReferenceViewController: UIViewController {
     // MARK: - Helpers
     
     func configureUI() {
-        self.view.layer.addSublayer(referenceVideoLayer)
+        self.view.layer.addSublayer(camVideoLayer)
         self.view.addSubview(self.stopButton)
-        self.view.addSubview(self.startButton)
         self.view.addSubview(self.progressBar)
             
-        self.timeObserver = self.referenceVideo.addPeriodicTimeObserver(forInterval: CMTimeMakeWithSeconds(0.01, preferredTimescale: 600), queue: DispatchQueue.main) { CMTime in
-            if self.referenceVideo.currentItem?.status == .readyToPlay {
-                let duration = CMTimeGetSeconds((self.referenceVideo.currentItem?.asset.duration)!)
-                let currentTime = CMTimeGetSeconds((self.referenceVideo.currentTime()))
+        self.timeObserver = self.camVideo.addPeriodicTimeObserver(forInterval: CMTimeMakeWithSeconds(0.01, preferredTimescale: 600), queue: DispatchQueue.main) { CMTime in
+            if self.camVideo.currentItem?.status == .readyToPlay {
+                let duration = CMTimeGetSeconds((self.camVideo.currentItem?.asset.duration)!)
+                let currentTime = CMTimeGetSeconds((self.camVideo.currentTime()))
                 let progress = Float(currentTime / duration) * 100
                 self.progressBar.value = progress
             }
@@ -98,13 +86,6 @@ class ExerciseReferenceViewController: UIViewController {
             $0.width.equalTo(100)
         }
         
-        self.startButton.snp.makeConstraints {
-            $0.left.equalTo(self.view.snp.left).offset(15)
-            $0.centerY.equalTo(self.stopButton.snp.centerY)
-            $0.right.equalTo(self.stopButton.snp.left).offset(-15)
-            $0.height.equalTo(50)
-        }
-        
         self.progressBar.snp.makeConstraints {
             $0.top.equalTo(self.view.snp.topMargin).offset(15)
             $0.left.equalTo(self.view.snp.left).offset(15)
@@ -115,18 +96,8 @@ class ExerciseReferenceViewController: UIViewController {
     // MARK: - Actions
     
     @objc func handleStopButtonTapped() {
-        self.referenceVideo.removeTimeObserver(self.timeObserver!)
+        self.camVideo.removeTimeObserver(self.timeObserver!)
         self.timeObserver = nil
-        self.navigationController?.popViewController(animated: true)
-    }
-    
-    @objc func handleStartButtonTapped() {
-        self.referenceVideo.pause()
-        let exerciseCamViewController = ExerciseCamViewController()
-        
-        exerciseCamViewController.hero.isEnabled = true
-        
-        self.navigationController?.hero.navigationAnimationType = .fade
-        self.navigationController?.pushViewController(exerciseCamViewController, animated: true)
+        self.navigationController?.popToViewController(ofClass: DetailViewController.self)
     }
 }
