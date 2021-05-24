514c5423af17ccbdbc45fb2b34cf2c203bc9f9dc
yabby <yabby1997@gmail.com>
2021-05-24 16:25:38 +0900

    ExerciseCamViewController에 Vision framework 도입, 손가락 끝을 인식하여 보여주도록 구현

--- PoC UIKit/ViewController/ExerciseCamViewController.swift
+++ PoC UIKit/ViewController/ExerciseCamViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 import AVKit
 import SnapKit
+import Vision
 
-class ExerciseCamViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+class ExerciseCamViewController: UIViewController {
     
     // MARK: - Properties
     
     private let videoDataOutputQueue = DispatchQueue(label: "CameraFeedDataOutput", qos: .userInteractive)
+    
+    private var pointsPath = UIBezierPath()
+    private var handPoseRequest = VNDetectHumanHandPoseRequest()
     
     lazy var captureSession: AVCaptureSession = {
         let session = AVCaptureSession()
@@ -47,6 +51,16 @@ class ExerciseCamViewController: UIViewController, AVCaptureVideoDataOutputSampl
         return layer
     }()
     
+    lazy var overlayLayer: CAShapeLayer = {
+        let layer = CAShapeLayer()
+        layer.frame = view.bounds
+        layer.lineWidth = 5
+        layer.fillColor = #colorLiteral(red: 0, green: 0.9810667634, blue: 0.5736914277, alpha: 1)
+        layer.lineCap = .round
+        layer.contentsGravity = .resizeAspectFill
+        return layer
+    }()
+    
     lazy var stopButton: UIButton = {
         let button = UIButton()
         button.clipsToBounds = true
@@ -68,12 +82,21 @@ class ExerciseCamViewController: UIViewController, AVCaptureVideoDataOutputSampl
         return slider
     }()
     
+    var scoreLabel: UILabel = {
+        let label = UILabel()
+        label.text = "85점"
+        label.font = UIFont.italicSystemFont(ofSize: 60)
+        label.textColor = .white
+        return label
+    }()
+    
     // MARK: - Lifecycles
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.configureUI()
+        handPoseRequest.maximumHandCount = 1
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -85,8 +108,10 @@ class ExerciseCamViewController: UIViewController, AVCaptureVideoDataOutputSampl
     
     func configureUI() {
         self.view.layer.addSublayer(captureLayer)
+        self.view.layer.addSublayer(overlayLayer)
         self.view.addSubview(self.stopButton)
         self.view.addSubview(self.progressBar)
+        self.view.addSubview(self.scoreLabel)
         
         self.stopButton.snp.makeConstraints {
             $0.right.equalTo(self.view.snp.right).offset(-15)
@@ -100,11 +125,104 @@ class ExerciseCamViewController: UIViewController, AVCaptureVideoDataOutputSampl
             $0.left.equalTo(self.view.snp.left).offset(15)
             $0.right.equalTo(self.view.snp.right).offset(-15)
         }
+        
+        self.scoreLabel.snp.makeConstraints {
+            $0.bottom.equalTo(self.stopButton.snp.top).offset(-15)
+            $0.right.equalTo(self.view.snp.right).offset(-15)
+        }
+    }
+    
+    func showPoints(_ points: [CGPoint], color: UIColor) {
+        pointsPath.removeAllPoints()
+        for point in points {
+            let path = UIBezierPath(ovalIn: CGRect(x: point.x, y: point.y, width: 10, height: 10))
+            pointsPath.append(path)
+        }
+        CATransaction.begin()
+        CATransaction.setDisableActions(true)
+        overlayLayer.path = pointsPath.cgPath
+        CATransaction.commit()
+    }
+    
+    func processPoints(thumbTip: CGPoint?, indexTip: CGPoint?, middleTip: CGPoint?, ringTip: CGPoint?, littleTip: CGPoint?) {
+        // 두 포인트가 다 잘 뽑혔는지 확인해서
+        guard let thumbPoint = thumbTip, let indexPoint = indexTip, let middlePoint = middleTip, let ringPoint = ringTip, let littlePoint = littleTip else {
+            // 둘다 없고, 없어진지 2초가 넘었으면 있던거 다 날려버려
+            self.showPoints([], color: .clear)
+            return
+        }
+        // 잘있으면 그 두 포인트를 실제 캡쳐가 이뤄진 영상에서의 좌표로 변환해주고 gestureProcessor의 processPointsPair를 호출한다.
+        let thumbPointConverted = self.captureLayer.layerPointConverted(fromCaptureDevicePoint: thumbPoint)
+        let indexPointConverted = self.captureLayer.layerPointConverted(fromCaptureDevicePoint: indexPoint)
+        let middlePointConverted = self.captureLayer.layerPointConverted(fromCaptureDevicePoint: middlePoint)
+        let ringPointConverted = self.captureLayer.layerPointConverted(fromCaptureDevicePoint: ringPoint)
+        let littlePointConverted = self.captureLayer.layerPointConverted(fromCaptureDevicePoint: littlePoint)
+        self.showPoints([thumbPointConverted, indexPointConverted, middlePointConverted, ringPointConverted, littlePointConverted], color: .green)
     }
     
     // MARK: - Actions
     
     @objc func handleStopButtonTapped() {
         self.navigationController?.popToViewController(ofClass: DetailViewController.self)
+    }
+}
+
+// MARK: - AVCaptureVideoDataOutputSampleBufferDelegate
+
+extension ExerciseCamViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    // 앞서 지정해놨던 버퍼에서 받아온 이미지를 가지고 이거저거 하는 부분
+    public func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
+        var thumbTip: CGPoint?
+        var indexTip: CGPoint?
+        var middleTip: CGPoint?
+        var ringTip: CGPoint?
+        var littleTip: CGPoint?
+        
+        // scope를 벗어나기 전 호출된다.
+        defer {
+            // 뽑힌 포인트로 processPoints를 호출한다.
+            DispatchQueue.main.sync {
+                self.processPoints(thumbTip: thumbTip,
+                                   indexTip: indexTip,
+                                   middleTip: middleTip,
+                                   ringTip: ringTip,
+                                   littleTip: littleTip)
+            }
+        }
+
+        let handler = VNImageRequestHandler(cmSampleBuffer: sampleBuffer, orientation: .up, options: [:])
+        do {
+            // request를 requestHandler에게 보내서 observation을 받아오고, 그로부터 원하는 포인트를 뽑아내는 부분.
+            try handler.perform([handPoseRequest])
+            guard let observation = handPoseRequest.results?.first else {
+                return
+            }
+            
+            print(observation)
+            
+            let thumbPoints = try observation.recognizedPoints(.thumb)
+            let indexFingerPoints = try observation.recognizedPoints(.indexFinger)
+            let middleTipPoint = try observation.recognizedPoint(.middleTip)
+            let ringTipPoint = try observation.recognizedPoint(.ringTip)
+            let littleTipPoint = try observation.recognizedPoint(.littleTip)
+            
+            guard let thumbTipPoint = thumbPoints[.thumbTip], let indexTipPoint = indexFingerPoints[.indexTip] else {
+                return
+            }
+            guard thumbTipPoint.confidence > 0.3 && indexTipPoint.confidence > 0.3 else {
+                return
+            }
+            
+            thumbTip = CGPoint(x: thumbTipPoint.location.x, y: 1 - thumbTipPoint.location.y)
+            indexTip = CGPoint(x: indexTipPoint.location.x, y: 1 - indexTipPoint.location.y)
+            middleTip = CGPoint(x: middleTipPoint.location.x, y: 1 - middleTipPoint.location.y)
+            ringTip = CGPoint(x: ringTipPoint.location.x, y: 1 - ringTipPoint.location.y)
+            littleTip = CGPoint(x: littleTipPoint.location.x, y: 1 - littleTipPoint.location.y)
+            
+        } catch {
+            captureSession.stopRunning()
+            return
+        }
     }
 }
